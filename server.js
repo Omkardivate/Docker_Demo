c7722b1ad776a062f208c20ae0b646c5a29698b2
Omkardivate <112164147+Omkardivate@users.noreply.github.com>
2024-01-03 23:16:23 +0530

    dockerfile added

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 var  express=require('express');
-
+var path = require('path');
 var app= express();
 
 // app.use(express.static('public'));
 
 app.get("/", (req,res)=>{
-    res.sendFile(path.join(__dirname, '/index.html'))
+    res.sendFile(path.join(__dirname, '/html/index.html'))
 })
 
-app.get("/welcome", (req,res)=>{
-    res.sendFile(path.join(__dirname, '/welcome.html'))
+app.get("/welcome.html", (req,res)=>{
+    res.sendFile(path.join(__dirname, '/html/welcome.html'))
 })
 
 app.listen(8080);
