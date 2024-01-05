0c3c348659030a1fd16361033f1ad0697dfeb0ce
Omkardivate <112164147+Omkardivate@users.noreply.github.com>
2024-01-05 22:00:47 +0530

    c1

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 var  express=require('express');
 var path = require('path');
+
 var app= express();
 
 // app.use(express.static('public'));
 
 app.get("/", (req,res)=>{
     res.sendFile(path.join(__dirname, '/html/index.html'))
+})
+
+app.get("/hello.html",(req,res)=>{
+    res.send("<h1> Hello </h1>");
 })
 
 app.get("/welcome.html", (req,res)=>{
